0d0779e1320803c86a96fdf1a121cbc6acbec50a
Chris Banta <chrisbanta1@gmail.com>
2020-03-16 17:47:36 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -1,9 +1,9 @@
-require 'pp'
+
 $LOAD_PATH.unshift(File.dirname(__FILE__))
 require 'directors_database'
 # Find a way to accumulate the :worldwide_grosses and return that Integer
 # using director_data as input
-pp 'director_data'
+
 
 def gross_for_director(director_data)
 movie_index = 0
